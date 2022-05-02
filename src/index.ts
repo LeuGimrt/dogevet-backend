cc05193233adcf13103b0492304df27722456353
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2022-05-02 17:26:59 -0500

    feat: fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from "./config/app";
 
 app.listen(app.get("port"), () => {
-  console.log(`Servidor iniciado en: ${app.get("port")}`);
+  console.log(`Servidor iniciado en el puerto: ${app.get("port")}`);
 });
